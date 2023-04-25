0af7bbbcc6c6faeed5e85ab61740293680e0ccda
CN-L <41163551+CN-L@users.noreply.github.com>
2023-04-25 16:24:25 +0800

    feat(columnlist): 父组件app.vue增加数据支持

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,8 +1,16 @@
 <template>
-  <div></div>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+    <img :src="column.avatar" :alt="column.title">
+    <h5>{{ column.title }}</h5>
+    <p>{{ column.description }}</p>
+    <a href="#">进入专栏</a>
+  </li>
+  </ul>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export interface ColumnProps {
   id: number,
   title: string,
