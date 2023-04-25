a3f373a6d3efed453f50d39b26832b2830b9543b
CN-L <41163551+CN-L@users.noreply.github.com>
2023-04-25 16:07:16 +0800

    feat(columnlist): 新增ColumnList组件

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number,
+  title: string,
+  avatar: string,
+  description: string
+}
+export default defineComponent({
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup () {
+    return {
+
+    }
+  }
+})
+</script>
