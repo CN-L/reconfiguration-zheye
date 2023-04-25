24ef64755cbb3bb420ac6f4920f4cf55f5158a13
CN-L <41163551+CN-L@users.noreply.github.com>
2023-04-25 16:46:09 +0800

    style(columnlist.vue): 样式修改 增加了替换图片 样式布局更改

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,20 +1,24 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-    <img :src="column.avatar" :alt="column.title">
-    <h5>{{ column.title }}</h5>
-    <p>{{ column.description }}</p>
-    <a href="#">进入专栏</a>
-  </li>
-  </ul>
+  <div class="row">
+    <div class="col-4 mb-4" v-for="column in ColumnList" :key="column.id">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img class="rounded-circle border border-light w-25 my-3" :src="column.avatar" :alt="column.title">
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+</div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 export interface ColumnProps {
   id: number,
   title: string,
-  avatar: string,
+  avatar?: string,
   description: string
 }
 export default defineComponent({
@@ -24,9 +28,17 @@ export default defineComponent({
       required: true
     }
   },
-  setup () {
+  setup (props) {
+    const ColumnList = computed(() => {
+      return props.list.map(item => {
+        if (!item.avatar) {
+          item.avatar = require('@/assets/colmun.webp')
+        }
+        return item
+      })
+    })
     return {
-
+      ColumnList
     }
   }
 })
