41b0399a17a51fd71dd710dd76fb57f9b7e8895c
CN-L <41163551+CN-L@users.noreply.github.com>
2023-05-27 23:14:07 +0800

    feat($add): 增加axios发送请求，设置了地址前缀和icode

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,29 @@ import { createApp } from 'vue'
 import store from '@/store/store'
 import App from './App.vue'
 import router from '@/router/index'
+import axios from 'axios'
+// 替换 baseURL
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+// 下面的 icode 值是从慕课网获取的 token 值，可以在课程右侧的项目接口校验码找到
+// Add a request interceptor
+const icode = '4CB97C9D7FB84B63s'
+axios.interceptors.request.use(function (config) {
+  config.params = { ...config.params, icode }
+  if (config.data instanceof FormData) {
+    config.data.append('icode', icode)
+  } else {
+    config.data = { ...config.data, icode }
+  }
+  // Do something before request is sent
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+axios.defaults.baseURL = 'http://apis.imooc.com'
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.mount('#app')
+axios.get('/api/column', { params: { key: 'hello' } }).then()
