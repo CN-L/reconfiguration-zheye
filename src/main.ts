ffab29176e7ba88abd46ac56fc71d528a1d0a233
CN-L <41163551+CN-L@users.noreply.github.com>
2023-06-01 19:53:34 +0800

    fix($fix): 修复darkmode-js依赖找不到的问题

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,6 @@ import store from '@/store/store'
 import App from './App.vue'
 import router from '@/router/index'
 import axios from 'axios'
-// 深色模式
-import Darkmode from 'darkmode-js'
-
-const options = {
-  bottom: '64px', // default: '32px'
-  right: '0', // default: '32px'
-  left: 'unset', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff', // default: '#fff'
-  buttonColorDark: '#100f2c', // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options)
-darkmode.showWidget()
 
 // 替换 baseURL
 axios.defaults.baseURL = 'http://apis.imooc.com/api/'
