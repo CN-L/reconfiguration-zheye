0ac7c0cc46e5a454b61405e4ed74cf5a6eb10aee
CN-L <41163551+CN-L@users.noreply.github.com>
2023-06-05 11:58:06 +0800

    feat($add): 增加背景图片 固定顶部header

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,22 @@
 <template>
-  <ValidateForm @form-submit="onFormSubmit" @clear-inputs="onClear">
+  <div class="login-page p-5 d-flex justify-content-center align-items-center">
+    <ValidateForm class="w-50 h-50" @form-submit="onFormSubmit" @clear-inputs="onClear">
       <div class="mb-3">
-        <label class="form-label">电子邮箱</label>
+        <label class="form-label text-body-tertiary">电子邮箱</label>
         <ValidateInput placeholder="请输入邮箱地址" v-model="emailVal" :rules="emailRules"></ValidateInput>
       </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <ValidateInput placeholder="请输入密码" v-model="passWord" :rules="passwordRules"></ValidateInput>
+      <div class="mb-5">
+        <label class="form-label text-body-tertiary">密码</label>
+        <ValidateInput type="password" placeholder="请输入密码" v-model="passWord" :rules="passwordRules"></ValidateInput>
       </div>
       <!-- 等同于v-slot:submit -->
       <template #submit>
-        <span class="btn btn-danger">submit</span>
+        <div class="text-center">
+          <span class="btn  btn-primary">submit</span>
+        </div>
       </template>
     </ValidateForm>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
@@ -69,3 +73,14 @@ export default defineComponent({
   }
 })
 </script>
+<style>
+.login-page {
+  background-image: url('@/assets/1.jpg');
+  background-size:cover;
+  background-repeat: no-repeat;
+  min-height: 500px;
+}
+.form-content {
+  opacity: 0.8;
+}
+</style>
